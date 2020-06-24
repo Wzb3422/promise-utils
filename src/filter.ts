36f22e25e0cb66ca44333e5fb231090543c7cf80
wzb <wzb3422@gmail.com>
2020-06-24 10:53:33 +0800

    :feat: setup jest

--- src/filter.ts
+++ src/filter.ts
@@ -1,12 +1,32 @@
-export function filter<T, V>(
-  input: T[],
-  predicate: (value: T, index?: number
-) => Promise<V>): Promise<T[]>;
-export function filter<T, V>(
+export function filter<T, V> (
+  input: readonly T[],
+  predicate: (value: T, index: number) => Promise<V>
+): Promise<T[]>;
+export function filter<T, V> (
+  input: readonly T[],
+  predicate: (value: T) => Promise<V>
+): Promise<T[]>;
+export function filter<T extends Object, V> (
   input: T,
   predicate: (value: T[keyof T], key: keyof T) => Promise<V>
-): Promise<T[keyof T][]> {
-  if (!input) {
-    Promise.resolve([])
-  } 
+): Promise<T[keyof T][]>;
+export function filter<T extends Object, V> (
+  input: T,
+  predicate: (value: T[keyof T]) => Promise<V>
+): Promise<T[keyof T][]>;
+// enforced by overload signature, validated by tests
+export function filter(input: any, predicate: any): any {
+  return new Promise ((resolve, reject) => {
+    if (!input) {
+      return []
+    }
+    const result: any[] = [];
+    for (let k in input) {
+      predicate(input[k]).then((truth: any) => {
+        if (truth) {
+          result.push(input[k])
+        }
+      })
+    }
+  })
 }
